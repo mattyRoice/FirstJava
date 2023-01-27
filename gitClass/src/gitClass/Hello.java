5838017ff23469f3b30c996af56b84048163d67e
DongHyun <kyu1148@gmail.com>
2023-01-27 15:26:36 +0900

    hello.java

--- gitClass/src/gitClass/Hello.java
+++ gitClass/src/gitClass/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
-		System.out.println("Hello2");
+		System.out.println("안녕하세요");
+		System.out.println("안녕하실까?");
 	}
 
 }
